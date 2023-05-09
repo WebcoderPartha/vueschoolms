67d464f606a10f8758b3e1933810373ba93ca90b
WebcoderPartha <parthadev76@gmail.com>
2023-05-09 16:25:33 +0600

    Employee Salary model and migration

---
+++ database/migrations/2023_05_09_155801_create_employee_salaries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee_salaries', function (Blueprint $table) {
+            $table->id();
+            $table->integer('employee_id');
+            $table->string('previous_salary')->nullable();
+            $table->string('present_salary')->nullable();
+            $table->string('increment_salary')->nullable();
+            $table->string('increment_salary_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee_salaries');
+    }
+};
