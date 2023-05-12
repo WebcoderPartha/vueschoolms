50e352bc4f126190169663682ce9d466a12aaaac
WebcoderPartha <parthadev76@gmail.com>
2023-05-12 11:02:54 +0600

    LeavePurpose and atttendance model creation

---
+++ app/Http/Controllers/Api/ManageEmployee/EmployeeLeaveController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\ManageEmployee;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EmployeeLeaveController extends Controller
+{
+    //
+}
