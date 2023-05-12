92987e88d13776522228126fe0043c560d54c7c6
WebcoderPartha <parthadev76@gmail.com>
2023-05-12 11:05:40 +0600

    Employee Leave model creation

---
+++ database/migrations/2023_05_12_110329_create_leaves_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leaves', function (Blueprint $table) {
+            $table->id();
+            $table->integer('employee_id');
+            $table->string('leave_purpose');
+            $table->string('leave_start_date');
+            $table->string('leave_end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leaves');
+    }
+};
